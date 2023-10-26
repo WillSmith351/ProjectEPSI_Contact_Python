17bb88b15aab142e7cd807c7bcec48d3a94fa4ad
WillSmith351 <williamcalvertsmith@gmail.com>
2023-10-26 15:51:05 +0200

    update

--- willfile.py
+++ willfile.py
@@ -10,7 +10,7 @@ fichier_json = 'databasewill.json'
 
 # Sauvegarde des informations de l'utilisateur
 def save_information_user():
-    if len(name_input.get()) <= 3 or len(surname_input.get()) <= 3:
+    if len(name_input.get()) <= 2 or len(surname_input.get()) <= 2:
         messagebox.showwarning("Attention", "Le nom et le prénom doivent comporter plus de 3 caractères.")
     else:
         contact = {
@@ -18,39 +18,50 @@ def save_information_user():
             "prenom": surname_input.get(),
             "telephone": phone_input.get()
         }
-        titre = title_input.get()
+        title = surname_input.get()
 
         with open(fichier_json, 'r') as file:
             data = json.load(file)
             
-        data[titre] = contact
+        data[title] = contact
         with open(fichier_json, 'w') as file_user:
-            json.dump(data, file_user)
-        messagebox.showinfo("Le contact a bien été enregistré.")
+            json.dump(data, file_user, indent=4)
+        messagebox.showinfo("Sauvegarde réussie", "Le contact a bien été enregistré.")
+
+
+# Fonction pour charger les données de la base de données JSON
+def charger_base_de_donnees():
+    try:
+        with open("database.json", "r") as fichier:
+            return json.load(fichier)
+    except FileNotFoundError:
+        return []
+    
+# Fonction pour sauvegarder les données de la base de données JSON
+def sauvegarder_base_de_donnees(data):
+    global fichier_json
+    with open(fichier_json, 'w') as fichier:
+        json.dump(data, fichier, indent=4)
+
 
 # Création des titres, labels, inputs et boutons.
 contact_title = tk.Label(screen, text="Entrez les informations du contact :")
 contact_title.pack()
 
-title_label = tk.Label(screen, text="Titre du contact :")
-title_label.pack()
-title_input = tk.Entry(screen)
-title_input.pack()
-
 name_label = tk.Label(screen, text="Nom :")
 name_label.pack()
-name_input = tk.Entry(screen)
-name_input.pack()
+name_input = tk.Entry(screen, font=("Arial", 12), bd=5)
+name_input.pack(pady=5)
 
 surname_label = tk.Label(screen, text="Prénom :")
 surname_label.pack()
-surname_input = tk.Entry(screen)
-surname_input.pack()
+surname_input = tk.Entry(screen, font=("Arial", 12), bd=5)
+surname_input.pack(pady=5)
 
 phone_label = tk.Label(screen, text="Numéro de téléphone :")
 phone_label.pack()
-phone_input = tk.Entry(screen)
-phone_input.pack()
+phone_input = tk.Entry(screen, font=("Arial", 12), bd=5)
+phone_input.pack(pady=5)
 
 # Création du bouton pour sauvegarder le contact. 
 contact_button = tk.Button(screen, text="Sauvegarder", command=save_information_user)
